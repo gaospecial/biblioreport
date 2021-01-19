4eddbdc013a94cd0a1e29c1c1ae61eb808544bf8
Chun-Hui Gao <gaospecial@gmail.com>
2021-01-19 17:25:05 +0800

    cumsum of nRecord

--- R/growth_curve.R
+++ R/growth_curve.R
@@ -6,6 +6,8 @@
 #' @export
 #'
 #' @examples
+#' data <- data.frame(year = 2001:2020, n = rpois(20, 100))
+#' growth_curve(data, aes(year, n))
 growth_curve <- function(data = NULL, mapping = aes()){
   ggplot2::ggplot(data = data, mapping = mapping) +
     ggplot2::geom_point() +
--- R/yearly_growth.R
+++ R/yearly_growth.R
@@ -1,8 +1,8 @@
 #' Find yearly growth of publications
 #'
 #' @param x a bibliometrixDB object
-#' @param estimate_current_year set to TRUE if your want an estimation of current year
-#'
+#' @param estimate_current_year set to TRUE if you want an estimation of current year
+#' @param cumulative set to TRUE if you want a cumulative sum
 #' @return a data frame (`yearly_growth()`) or ggplot object (`yearly_growth_curve()`)
 #' @export
 #' @name yearly_growth
@@ -12,23 +12,28 @@
 #' d1 <- yearly_growth(garfield, estimate_current_year = TRUE)
 #' yearly_growth_curve(d1)
 #'
-yearly_growth <- function(x, estimate_current_year = FALSE, na.rm = TRUE){
+yearly_growth <- function(x,
+                          estimate_current_year = FALSE,
+                          na.rm = TRUE,
+                          cumulative = FALSE){
 
   d <- x %>% dplyr::group_by(.data$PY) %>%
     dplyr::count() %>%
     dplyr::mutate_all(as.numeric())
   if (na.rm) d <- d %>% dplyr::filter(!is.na(.data$PY))
-  if (!estimate_current_year){
-    return(d)
-  } else {
+  if (estimate_current_year){
     this_year <- lubridate::year(Sys.time())
     d1 <- d %>% dplyr::filter(PY != this_year)
     d2 <-  d %>% dplyr::filter(PY == this_year)
     model <- loess(n ~ PY, d1, surface = "direct")
     d2$n <- predict(model, d2)
     d <- rbind(d1,d2)
-    return(d)
   }
+  if (cumulative){
+    d <- d[order(d$PY),] %>%
+      dplyr::mutate(n = cumsum(n))
+  }
+  return(d)
 }
 
 #' @rdname yearly_growth
